bc28d7e41234b17d80ecc8605d35846c31b1e19b
leegakyeong <alaska7132@naver.com>
2024-11-14 12:40:57 +0900

    Implement glow animation for 'good' effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react'
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Scene, Mesh, Animation, CircleEase, EasingFunction, StandardMaterial, Color3 } from '@babylonjs/core'
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Scene, Mesh, Animation, CircleEase, EasingFunction, StandardMaterial, Color3, GlowLayer } from '@babylonjs/core'
 import earcut from 'earcut'
 import { Input } from '@/components/ui/input'
 import SceneComponent from './components/3d/SceneComponent'
@@ -25,9 +25,18 @@ function App() {
 
     userInput.split(' ').forEach((word, i) => {
       switch (word) {
-        case 'good':
+        case 'good': {
           console.log('good')
+          const glow = new GlowLayer('glow', scene)
+          glow.customEmissiveColorSelector = function (mesh, subMesh, material, result) {
+            if (mesh.name === "letterMesh") {
+              result.set(1, 1, 0, 1);
+            } else {
+              result.set(0, 0, 0, 0);
+            }
+          }
           break
+        }
         case 'sad':
           console.log('sad')
           break
@@ -47,7 +56,7 @@ function App() {
           fontData,
           {
             size: 2,
-            resolution: 64,
+            resolution: 10,
             depth: 1,
           },
           scene,
@@ -62,7 +71,8 @@ function App() {
         x += 2 // letterMesh.getBoundingInfo().boundingBox
 
         const letterMaterial = new StandardMaterial('letterMaterial', scene)
-        letterMaterial.diffuseColor = new Color3(1, 1, 0)
+        // letterMaterial.diffuseColor = new Color3(1, 1, 0)
+        letterMaterial.emissiveColor = new Color3(1, 0.5, 1)
         letterMesh.material = letterMaterial
 
         // 마지막 글자면
@@ -76,7 +86,7 @@ function App() {
           )
           const keyframes = [
             { frame: 0, value: 0 },
-            { frame: 120, value: 1 },
+            { frame: 10, value: 1 },
           ]
           letterAppearanceAnimation.setKeys(keyframes)
 
@@ -85,7 +95,7 @@ function App() {
           letterAppearanceAnimation.setEasingFunction(easingFunction)
 
           letterMesh.animations.push(letterAppearanceAnimation)
-          scene.beginAnimation(letterMesh, 0, 120, true)
+          scene.beginAnimation(letterMesh, 0, 120, false)
         }
       })
     })
