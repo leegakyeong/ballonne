72f488011052ee82d4979a0492d48130f650e096
leegakyeong <alaska7132@naver.com>
2024-11-19 22:51:56 +0900

    Take createLetterMesh and positionLetterMesh out of useEffect

--- src/components/3d/SceneComponent.tsx
+++ src/components/3d/SceneComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useCallback } from 'react'
 import * as BABYLON from '@babylonjs/core'
 import { Compiler, Font, TextMeshBuilder } from 'babylon.font'
 import opentype from 'opentype.js'
@@ -37,6 +37,157 @@ export default function SceneComponent({
   const engineRef = useRef<BABYLON.Engine>()
   const textMeshRef = useRef<BABYLON.Mesh[]>([])
   const letterPosRef = useRef({ x: 0, y: 0 })
+
+  const createLetterMesh = useCallback((letter: string, font: Font, builder: TextMeshBuilder, scene: BABYLON.Scene) => {
+    const frontMesh: BABYLON.Mesh = builder.create(
+      {
+        font,
+        text: letter === ' ' ? 'l' : letter,
+        size: extrusionOptions.scale,
+        ppc: 5,
+        eps: 0.001,
+        // plus `BABYLON.MeshBuilder.CreatePolygon` options
+        // depth,
+        // sideOrientation,
+        // faceColors,
+        // faceUV,
+        // backUVs,
+        // frontUVs,
+        // updatable,
+       },
+      scene,
+    )
+    const positions = frontMesh.getVerticesData(BABYLON.VertexBuffer.PositionKind)
+    if (!positions) return
+    // paths도 닫아 줘야 하는데...
+    const polygonPath = []
+    for (let i = 0; i < positions.length; i += 3) {
+      polygonPath.push(new BABYLON.Vector3(positions[i], positions[i + 2], 0))
+    }
+    // polygonPath.push(polygonPath[0]) // 폴리곤을 닫아야 함. 이 부분 고치기!
+
+    // frontMesh.setEnabled(false)
+    frontMesh.rotate(new BABYLON.Vector3(1, 0, 0), -Math.PI / 2)
+    // frontMesh.translate(new BABYLON.Vector3(1, 0, 0), 1)
+
+    // Bevel 경로 생성
+    const { depth: bevelDepth, segments: bevelSegments } = bevelOptions
+    // const beveledPaths = createBeveledPaths(polygonPath, bevelDepth, bevelSize, bevelSegments, bevelDepth);
+    const beveledPaths = createBeveledPaths(polygonPath, bevelDepth, 0.2, bevelSegments)
+
+    const bevelMesh = BABYLON.MeshBuilder.CreateRibbon(
+      "bevelMesh",
+      {
+        pathArray: beveledPaths,
+        closeArray: true,
+        closePath: true
+      },
+      scene
+    );
+
+    const sideMesh = BABYLON.ExtrudeShape('letterMesh', {
+      shape: polygonPath,
+      path: [new BABYLON.Vector3(0, 0, bevelDepth), new BABYLON.Vector3(0, 0, extrusionOptions.depth - bevelDepth)],
+      // scale: extrusionOptions.scale,
+      rotation: Math.PI * extrusionOptions.rotation,
+      closeShape: true,
+      closePath: true,
+      // cap: BABYLON.Mesh.CAP_ALL,
+    })
+    // bevelDepth가 extrusion.depth보다 커지면 안 됨!!
+
+    const letterMesh = BABYLON.Mesh.MergeMeshes([frontMesh, bevelMesh, sideMesh], true)
+    frontMesh.isEnabled(false)
+    bevelMesh.isEnabled(false)
+    sideMesh.isEnabled(false)
+
+    if (!letterMesh) return
+
+    // const letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
+    let letterMaterial
+    switch (materialType) {
+      case 'StandardMaterial': {
+        const { diffuseColor, specularColor, emissiveColor, ambientColor } = standardMaterialOptions
+
+        letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
+
+        letterMaterial.diffuseColor = diffuseColor
+        letterMaterial.specularColor = specularColor
+        letterMaterial.emissiveColor = emissiveColor
+        letterMaterial.ambientColor = ambientColor
+
+        break
+      }
+      case 'PBRMaterial': {
+        const { albedoColor, metallic, roughness, alpha, refraction, translucency } = pbrMaterialOptions
+
+        letterMaterial = new BABYLON.PBRMaterial('letterMaterial', scene)
+
+        letterMaterial.albedoColor = albedoColor
+
+        letterMaterial.metallic = metallic
+        letterMaterial.roughness = roughness
+        letterMaterial.alpha = alpha
+
+        letterMaterial.subSurface.isRefractionEnabled = true
+        letterMaterial.subSurface.refractionIntensity = refraction
+
+        letterMaterial.subSurface.isTranslucencyEnabled = true
+        letterMaterial.subSurface.translucencyIntensity = translucency
+
+        break
+      }
+      case 'CustomMaterial': {
+        letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
+        break
+      }
+      default: {
+        letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
+        break
+      }
+    }
+    letterMesh.material = letterMaterial;
+
+    if (letter === ' ') letterMaterial.alpha = 0
+
+    return letterMesh
+  }, [bevelOptions, extrusionOptions, materialType, pbrMaterialOptions, standardMaterialOptions])
+
+  function positionLetterMesh(letter: string, letterMesh: BABYLON.Mesh, scene: BABYLON.Scene, isDelete=false) {
+    const maximum = letterMesh.getBoundingInfo().boundingBox.maximum
+    const minimum = letterMesh.getBoundingInfo().boundingBox.minimum
+    const letterWidth = maximum.subtract(minimum).x
+
+    const letterSpacing = 0.1
+    const translationX = (letterWidth / 2 + letterSpacing) * (isDelete ? -1 : 1)
+
+    letterPosRef.current.x += translationX
+
+    letterMesh.position.x = letterPosRef.current.x
+    // letterMesh.position.y = y
+
+    letterPosRef.current.x += translationX
+
+    const letterAppearanceAnimation = new BABYLON.Animation(
+      'letterAppearanceAnimation',
+      'material.alpha',
+      30,
+      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+      BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE,
+    )
+
+    letterAppearanceAnimation.setKeys([
+      { frame: 0, value: isDelete ? 1 : 0 },
+      { frame: 10, value: isDelete ? 0 : 1 },
+    ])
+
+    const easingFunction = new BABYLON.CircleEase()
+    easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT)
+    letterAppearanceAnimation.setEasingFunction(easingFunction)
+
+    if (letter !== ' ') letterMesh.animations.push(letterAppearanceAnimation)
+    scene.beginAnimation(letterMesh, 0, 120, false, 1, isDelete ? () => letterMesh.dispose() : undefined)
+  }
 
   // set up basic engine and scene
   useEffect(() => {
@@ -81,7 +232,7 @@ export default function SceneComponent({
   }, [antialias])
 
   useEffect(() => {
-    async function updateTextMesh(scene: BABYLON.Scene, text:string) {
+    async function updateTextMesh(scene: BABYLON.Scene, text: string) {
       const compiler = await Compiler.Build(wasmUrl);
       const font = await Font.Install("/src/assets/NotoSansKR-Regular.ttf", compiler, opentype);
       const builder = new TextMeshBuilder(BABYLON, earcut)
@@ -96,7 +247,7 @@ export default function SceneComponent({
 
         if (!letterMesh) return
 
-        positionLetter(lastLetter, letterMesh, scene)
+        positionLetterMesh(lastLetter, letterMesh, scene)
 
         textMeshRef.current.push(letterMesh)
       } else if (text < prevText) {
@@ -104,7 +255,7 @@ export default function SceneComponent({
 
         if (!letterMesh) return
 
-        positionLetter(prevText[prevText.length - 1], letterMesh, scene, true)
+        positionLetterMesh(prevText[prevText.length - 1], letterMesh, scene, true)
 
         textMeshRef.current.pop()
       } else {
@@ -169,161 +320,10 @@ export default function SceneComponent({
       })
     }
 
-    function createLetterMesh(letter: string, font: Font, builder: TextMeshBuilder, scene: BABYLON.Scene) {
-      const frontMesh: BABYLON.Mesh = builder.create(
-        {
-          font,
-          text: letter === ' ' ? 'l' : letter,
-          size: extrusionOptions.scale,
-          ppc: 5,
-          eps: 0.001,
-          // plus `BABYLON.MeshBuilder.CreatePolygon` options
-          // depth,
-          // sideOrientation,
-          // faceColors,
-          // faceUV,
-          // backUVs,
-          // frontUVs,
-          // updatable,
-         },
-        scene,
-      )
-      const positions = frontMesh.getVerticesData(BABYLON.VertexBuffer.PositionKind)
-      if (!positions) return
-      // paths도 닫아 줘야 하는데...
-      const polygonPath = []
-      for (let i = 0; i < positions.length; i += 3) {
-        polygonPath.push(new BABYLON.Vector3(positions[i], positions[i + 2], 0))
-      }
-      // polygonPath.push(polygonPath[0]) // 폴리곤을 닫아야 함. 이 부분 고치기!
-
-      // frontMesh.setEnabled(false)
-      frontMesh.rotate(new BABYLON.Vector3(1, 0, 0), -Math.PI / 2)
-      // frontMesh.translate(new BABYLON.Vector3(1, 0, 0), 1)
-
-      // Bevel 경로 생성
-      const { depth: bevelDepth, segments: bevelSegments } = bevelOptions
-      // const beveledPaths = createBeveledPaths(polygonPath, bevelDepth, bevelSize, bevelSegments, bevelDepth);
-      const beveledPaths = createBeveledPaths(polygonPath, bevelDepth, 0.2, bevelSegments)
-
-      const bevelMesh = BABYLON.MeshBuilder.CreateRibbon(
-        "bevelMesh",
-        {
-          pathArray: beveledPaths,
-          closeArray: true,
-          closePath: true
-        },
-        scene
-      );
-
-      const sideMesh = BABYLON.ExtrudeShape('letterMesh', {
-        shape: polygonPath,
-        path: [new BABYLON.Vector3(0, 0, bevelDepth), new BABYLON.Vector3(0, 0, extrusionOptions.depth - bevelDepth)],
-        // scale: extrusionOptions.scale,
-        rotation: Math.PI * extrusionOptions.rotation,
-        closeShape: true,
-        closePath: true,
-        // cap: BABYLON.Mesh.CAP_ALL,
-      })
-      // bevelDepth가 extrusion.depth보다 커지면 안 됨!!
-
-      const letterMesh = BABYLON.Mesh.MergeMeshes([frontMesh, bevelMesh, sideMesh], true)
-      frontMesh.isEnabled(false)
-      bevelMesh.isEnabled(false)
-      sideMesh.isEnabled(false)
-
-      if (!letterMesh) return
-
-      // const letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
-      let letterMaterial
-      switch (materialType) {
-        case 'StandardMaterial': {
-          const { diffuseColor, specularColor, emissiveColor, ambientColor } = standardMaterialOptions
-
-          letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
-
-          letterMaterial.diffuseColor = diffuseColor
-          letterMaterial.specularColor = specularColor
-          letterMaterial.emissiveColor = emissiveColor
-          letterMaterial.ambientColor = ambientColor
-
-          break
-        }
-        case 'PBRMaterial': {
-          const { albedoColor, metallic, roughness, alpha, refraction, translucency } = pbrMaterialOptions
-
-          letterMaterial = new BABYLON.PBRMaterial('letterMaterial', scene)
-
-          letterMaterial.albedoColor = albedoColor
-
-          letterMaterial.metallic = metallic
-          letterMaterial.roughness = roughness
-          letterMaterial.alpha = alpha
-
-          letterMaterial.subSurface.isRefractionEnabled = true
-          letterMaterial.subSurface.refractionIntensity = refraction
-
-          letterMaterial.subSurface.isTranslucencyEnabled = true
-          letterMaterial.subSurface.translucencyIntensity = translucency
-
-          break
-        }
-        case 'CustomMaterial': {
-          letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
-          break
-        }
-        default: {
-          letterMaterial = new BABYLON.StandardMaterial('letterMaterial', scene)
-          break
-        }
-      }
-      letterMesh.material = letterMaterial;
-
-      if (letter === ' ') letterMaterial.alpha = 0
-
-      return letterMesh
-    }
-
-    function positionLetter(letter: string, letterMesh: BABYLON.Mesh, scene: BABYLON.Scene, isDelete=false) {
-      const maximum = letterMesh.getBoundingInfo().boundingBox.maximum
-      const minimum = letterMesh.getBoundingInfo().boundingBox.minimum
-      const letterWidth = maximum.subtract(minimum).x
-
-      const letterSpacing = 0.1
-      const translationX = (letterWidth / 2 + letterSpacing) * (isDelete ? -1 : 1)
-
-      letterPosRef.current.x += translationX
-
-      letterMesh.position.x = letterPosRef.current.x
-      // letterMesh.position.y = y
-
-      letterPosRef.current.x += translationX
-
-      const letterAppearanceAnimation = new BABYLON.Animation(
-        'letterAppearanceAnimation',
-        'material.alpha',
-        30,
-        BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE,
-      )
-
-      letterAppearanceAnimation.setKeys([
-        { frame: 0, value: isDelete ? 1 : 0 },
-        { frame: 10, value: isDelete ? 0 : 1 },
-      ])
-
-      const easingFunction = new BABYLON.CircleEase()
-      easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT)
-      letterAppearanceAnimation.setEasingFunction(easingFunction)
-
-      if (letter !== ' ') letterMesh.animations.push(letterAppearanceAnimation)
-      scene.beginAnimation(letterMesh, 0, 120, false, 1, isDelete ? () => letterMesh.dispose() : undefined)
-    }
-
     if (sceneRef.current) {
       updateTextMesh(sceneRef.current, text);
     }
-  }, [text, prevText, bevelOptions, extrusionOptions, materialType, standardMaterialOptions, pbrMaterialOptions])
+  }, [text, prevText, materialType, createLetterMesh])
 
   return <canvas ref={canvasRef} {...rest} className="w-full flex-1" /> // 리사이징 했을 때 버그 해결해야 함
 }
